933380ec6c8aee4a5fe7fd5e9c5733595d9fe7ec
Patrick Wiley <wileypatrick36@yahoo.com>
2021-12-21 13:35:49 +0100

    Create Project

---
+++ SoundsBinder/SoundsBinder/Sources/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SoundsBinder
+//
+//  Created by Patrick Wiley on 21.12.21.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    
+}
+
