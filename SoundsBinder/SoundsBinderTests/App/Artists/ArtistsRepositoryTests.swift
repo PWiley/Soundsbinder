2dc554edde033e1041a3f8b97534edc283322399
Patrick Wiley <wileypatrick36@yahoo.com>
2022-02-01 18:15:09 +0100

    Wip

---
+++ SoundsBinder/SoundsBinderTests/App/Artists/ArtistsRepositoryTests.swift
@@ -0,0 +1,21 @@
+//
+//  ArtistsRepositoryTests.swift
+//  SoundsBinderTests
+//
+//  Created by Patrick Wiley on 18.01.22.
+//
+
+import Foundation
+import XCTest
+@testable import SoundsBinder
+
+final class ArtistsRepositoryTests: XCTestCase {
+  
+    // MARK: - Properties
+    
+    private var repository: ArtistsRepository!
+    
+    // MARK: - Tests
+    
+    
+}
