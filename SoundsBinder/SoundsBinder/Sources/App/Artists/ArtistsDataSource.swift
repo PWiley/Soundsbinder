b2a60d0b790ea7e7d2cdbc25e801736d51a9fbf6
Patrick Wiley <wileypatrick36@yahoo.com>
2022-02-15 11:17:20 +0100

    WIP ArtistsDataSource <- CollectionViewCell

--- SoundsBinder/SoundsBinder/Sources/App/Artists/ArtistsDataSource.swift
+++ SoundsBinder/SoundsBinder/Sources/App/Artists/ArtistsDataSource.swift
@@ -9,9 +9,13 @@ import UIKit
 
 final class ArtistsDataSource: NSObject, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    // MARK: - Properties
+    // MARK: - Private Properties
     
     private var catalogArtists: [VisibleArtist] = []
+    
+    // MARK: - Public Properties
+    
+    var didSelectItemAtIndex: ((Int) -> Void)?
     
     // MARK: - Initialisers
     
@@ -24,21 +28,33 @@ final class ArtistsDataSource: NSObject, UICollectionViewDataSource, UICollectio
     // MARK: - UICollectionView Methods
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //return catalogArtists.count// How many cells to display
-        return 2// How many cells to display
+        return catalogArtists.count// How many cells to display
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let artistCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ArtistCell
-        //artistCell.configure(with: catalogArtists[indexPath.row])
-        artistCell.backgroundColor = .brown
+        artistCell.configure(with: catalogArtists[indexPath.row])
+       
         return artistCell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //        viewModel.didPressCellForDetail(indexPath: indexPath.item)
+        let index = indexPath.item
+        guard indexPath.item < catalogArtists.count else {
+            fatalError()
+        }
+        didSelectItemAtIndex?(index)
     }
     
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        let height = collectionView.frame.size.height
+//        let width = collectionView.frame.size.width
+//        // in case you you want the cell to be 40% of your controllers view
+//        return CGSize(width: width * 0.1, height: height * 0.1)
+//    }
+    
+
     func update(with catalogArtists: [Artist]) {
         self.catalogArtists = catalogArtists.map {
             VisibleArtist(artist: $0)
